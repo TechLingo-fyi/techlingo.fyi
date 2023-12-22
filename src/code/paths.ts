46c7b12118d9637a7ac28fbe6ab5098ec9ad3fcd
Antonio Feregrino <antonio.feregrino@gmail.com>
2023-12-22 17:49:34 +0000

    Unify path selection

---
+++ src/code/paths.ts
@@ -0,0 +1,34 @@
+import type { Lingo } from "@/TypeLingo";
+import { getCollection } from "astro:content";
+
+export async function getLingoPaths() {
+  const lingos = (await getCollection("lingos")).map((lingo) => {
+    return lingo.data as Lingo;
+  });
+
+  const list = lingos.map((lingo) => {
+    const languages = new Array();
+    lingo.definitions.forEach((element) => {
+      languages.push(element.language);
+    });
+
+    return languages.map((language) => {
+      return {
+        params: {
+          lingo: lingo.term,
+          language: language,
+        },
+        props: {
+          data: lingo,
+          relatedLingos: lingos
+            .filter((l) => l.term !== lingo.term)
+            .sort(() => 0.5 - Math.random())
+            .slice(0, 3)
+            .map((l) => l.term),
+        },
+      };
+    });
+  });
+
+  return list.flat();
+}
