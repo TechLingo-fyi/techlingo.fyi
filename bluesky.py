139f20482c7181131d4c100fc5523ea2a3911a9c
Antonio Feregrino <antonio.feregrino@gmail.com>
2024-12-16 08:07:19 +0000

    Add Bluesky and new lingo

--- bluesky.py
+++ bluesky.py
@@ -3,7 +3,7 @@ import random
 from pathlib import Path
 
 import requests
-from atproto import Client, client_utils
+from atproto import Client, client_utils, models
 import os
 
 all_lingos = list(Path("src/content/lingos").glob("*.json"))
@@ -33,14 +33,25 @@ data = {
 
 image = requests.get(f"https://og.techlingo.fyi/api", params=data)
 
-message = f'{random_lingo_data["term"]}: {random_lingo_definition["definition"]}'
-
-tb = client_utils.TextBuilder()
-tb.text(message)
-tb.text("\n\n")
-tb.link(url, url)
 
 client = Client()
 client.login("techlingo.fyi", os.environ["BLUESKY_PASSWORD"])
 
-client.send_image(tb, image=image.content, image_alt=message, langs=langs)
+image_blob = client.upload_blob(image.content)
+
+main = models.AppBskyEmbedExternal.Main(
+    external=models.AppBskyEmbedExternal.External(
+        description=random_lingo_definition["definition"], title=random_lingo_data["term"], uri=url, thumb=image_blob.blob
+    )
+)
+
+tb = client_utils.TextBuilder()
+
+tb.link(random_lingo_data["term"], url)
+tb.text(": ")
+tb.text(random_lingo_definition["definition"])
+
+message = f'{random_lingo_data["term"]}: {random_lingo_definition["definition"]}'
+
+client.send_post(tb, embed=main)
+
