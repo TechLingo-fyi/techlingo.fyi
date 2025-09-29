d5fa5ed020faf373d107ab9d9499910e596b4503
Antonio Feregrino <antonio.feregrino@gmail.com>
2025-09-29 06:31:41 +0100

    Fix formatting

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -10,4 +10,16 @@ export default {
       },
     },
   ],
+  // Global options
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "lf",
 };
